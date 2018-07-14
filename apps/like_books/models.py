9a4e00aea37ec23661d8480962edede15dea159a
ste ste <steelst0@gmail.com>
2018-07-14 10:49:52 -0700

    midway through likes/books

---
+++ apps/like_books/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+from django.db import models
+# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    # The Many to Many link
+    books = models.ManyToManyField(Book, related_name="liked_books")
+    # The One to Many Link
+    book = models.ForeignKey(Book, related_name="uploaded_books")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
